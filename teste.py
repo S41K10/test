fa8ba5ea0a389c2f3be3a27facbcadaf7228e21d
26Tassiofernandes <86972667+26Tassiofernandes@users.noreply.github.com>
2021-08-26 03:12:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -15,11 +15,13 @@ with open("C:\\Users\\augus\OneDrive\Documentos\\archive\\campeonato-brasileiro-
         data = tokens[2]
         mandante = tokens[5].lower()
         visitante = tokens[6].lower()
+        golsMandante = tokens[9]
+        golsVisitante = tokens[10]
 
         ano = data.split('-')[0]
 
         if (ano == "2014" or ano == "2015") and (visitante == time or mandante == time):
             print("-"*100)
-            print(f"{data}\t|{mandante}\t|{visitante}")
+            print(f"{data}\t|{mandante}= {golsMandante}\t|{visitante}= {golsVisitante}")
 
 
