f525023e96551636ca56e1b6c44bf31fc5a7cf66
S41K10 <89564462+S41K10@users.noreply.github.com>
2021-08-26 02:10:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,2 +1,23 @@
-print("teste")
-print('jair')
+import csv
+
+print("-"*100)
+print("Data\tMandante\tVisitante")
+print("-"*100)
+
+with open("C:\\Users\\augus\OneDrive\Documentos\\archive\\campeonato-brasileiro-full.csv", 'r') as arquivo:
+
+    results = csv.reader(arquivo)
+
+    for line in results:
+        tokens = line[0].split(';')
+
+        data = tokens[2]
+        mandante = tokens[5]
+        visitante = tokens[6]
+
+        if(data.split('-')[0] == "2014"):
+            print("-"*100)
+            print(f"{data}\t|{mandante}\t|{visitante}")
+
+
+
